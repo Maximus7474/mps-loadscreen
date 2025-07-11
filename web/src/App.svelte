1ef0a32c8e6bd56733a2869058263a456c877aeb
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-11 22:23:01 +0200

    refactor(web): remove the functions used for ingame NUI -> not needed due to it being a loading screen

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
-  import VisibilityProvider from './providers/VisibilityProvider.svelte';
   import HelloWorld from './components/HelloWorld.svelte';
-  import { debugData } from './utils/debugData';
 
-  debugData([
-    {
-      action: 'setVisible',
-      data: true,
-    },
-  ]);
 </script>
 
 <main>
-  <VisibilityProvider>
-    <HelloWorld />
-  </VisibilityProvider>
+  <HelloWorld />
 </main>
--- web/src/components/HelloWorld.svelte
+++ web/src/components/HelloWorld.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import { visibility } from '../store/stores';
-  import { fetchNui } from '../utils/fetchNui';
-
   interface ReturnData {
     x: number;
     y: number;
@@ -11,18 +8,7 @@
   let clientData: ReturnData;
 
   const handleClientData = () => {
-    fetchNui('getClientData')
-      .then((returnData) => {
-        clientData = returnData;
-      })
-      .catch((e) => {
-        clientData = { x: 100, y: 100, z: 100 };
-      });
-  };
-
-  const closeDialog = () => {
-    visibility.set(false);
-    fetchNui('hideUI');
+    clientData = { x: 100, y: 100, z: 100 }
   };
 </script>
 
@@ -31,7 +17,6 @@
   <div style="margin: 0.3vh 0;">Player coords: {JSON.stringify(clientData) || ''}</div>
   <div class="btn-wrapper">
     <button on:click={handleClientData}> Get client coords </button>
-    <button on:click={closeDialog}>Close popup</button>
   </div>
   <span>Or press the escape key!</span>
 </div>
